8760757b580faf62999893d105972ced368ffe77
Anna Mikhaylova <anna.micj@gmail.com>
2023-04-28 14:56:44 +0300

    add patience as argument

--- multimodal_perceiver/wandb_config.py
+++ multimodal_perceiver/wandb_config.py
@@ -8,8 +8,10 @@ from utility import train_net
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("-n", "--name", default="va-perceiver", type=str, help="project name for wandb")
-parser.add_argument("--lr", default=3e-3, help="learning rate")
+parser.add_argument("--lr", type=float, default=3e-3, help="learning rate")
 parser.add_argument("--batch_size", default=4, type=int, help="batch size")
+parser.add_argument("--patience", default=3, type=int, help="model patience")
+parser.add_argument("--epoch", default=10, type=int, help="epochs")
 
 def create_config(args):
     sweep_config = {'method': 'grid'}
@@ -23,7 +25,7 @@ def create_config(args):
     
     parameters_dict = {
         'seed': {
-            'values': [42, 13]
+            'values': [13]
         }
     }
 
@@ -31,13 +33,16 @@ def create_config(args):
 
     parameters_dict.update({
         'epochs': {
-            'value': 3
+            'value': args.epoch
         },
         'batch_size': {
             'value': args.batch_size
         },
         'lr': {
             'value': args.lr
+        },
+        'patience': {
+            'value': args.patience
         }
     })
 
